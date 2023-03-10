c3c9ebeeab0e38f806fd6c815ffbc8b1da60b31b
Nilkdo <nicolap02m@gmail.com>
2023-03-09 21:18:32 -0500

    nuevos cambios en el envio de correos

--- sendemail.php
+++ sendemail.php
@@ -2,9 +2,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php'
-require 'PHPMailer/src/PHPMailer,php'
-require 'PHPMailer/src/SMTP.php'
+require 'PHPMailer/PHPMailer/src/Exception.php'
+require 'PHPMailer/PHPMailer/src/PHPMailer,php'
+require 'PHPMailer/PHPMailer/src/SMTP.php'
 
 $mail = new PHPMailer(true):
 
