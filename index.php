fa95b79487fc6e22f14125b88481ffc31c53b633
Nilkdo <nicolap02m@gmail.com>
2023-03-16 18:52:10 -0500

    actualizacion de envio de correos

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/PHPMailer/src/Exception.php';
+require 'PHPMailer/PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/PHPMailer/src/SMTP.php';
+188;
 
 
 
-$connect = mysqli_connect('localhost', 'usb', 'usb2022', 'formulatio');
+$connect = mysqli_connect('localhost', 'admin', '12345', 'formulario');
 
 $email = isset( $_POST['email'] ) ? $_POST['email'] : '';
 $message = isset( $_POST['message'] ) ? $_POST['message'] : '';
@@ -29,26 +36,45 @@ if (count($_POST))
     if ($errors == 0)
     {
 
-        $query = 'INSERT INTO contact (
+        $query = 'INSERT INTO contacto (
                 email,
-                message
+                mensaje
             ) VALUES (
                 "'.addslashes($_POST['email']).'",
                 "'.addslashes($_POST['message']).'"
             )';
         mysqli_query($connect, $query);
 
-        $message = 'You have received a contact form submission:
+        $message = 'You have received a contact form submission';
             
-Email: '.$_POST['email'].'
-Message: '.$_POST['email'];
+//Email: '.$_POST['email'].'
+//Message: '.$_POST['email'];
 
-        mail( 'poveda.geovanny@hotmail.com', 
-            'Contact Form Cubmission',
-            $message );
 
-        header('Location: thankyou.html');
-        die();
+$email = new PHPMailer();
+try{
+
+$email = new PHPMailer();
+$email->isSMTP();
+$email->Host = 'smtp.mailtrap.io';
+$email->SMTPAuth = true;
+$email->Port = 2525;
+$email->Username = 'bc7e5e699c184c';
+$email->Password = '3a8b17d95a14a6';
+
+$email->setFrom('nicolas02m@gmail.com');
+$email->addAddress('nicolap02m@gmail.com');
+
+$email->isHTML(true);
+$email->Subject ='contacto desde el formulario';
+$email->Body    ='este es el contenido del mensaje <b>en negrita!</b>1';
+$email->AltBody ='Este es el contenido del mensaje en texto plano';
+
+$email->send();
+echo'El mensaje ha sido enviado';
+}catch(Exception $e) {
+    echo'el mensaje no se a podido enviar',$email->ErrorInfo;
+}
 
     }
 }
